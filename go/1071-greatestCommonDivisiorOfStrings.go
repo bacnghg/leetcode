84dcf1a581169071ca6d2c3021a078a2657081df
bacnh7427 <bacnh7427@co-well.com.vn>
2023-02-09 10:40:26 +0700

    fixbug problem for 1071

--- go/1071-greatestCommonDivisiorOfStrings.go
+++ go/1071-greatestCommonDivisiorOfStrings.go
@@ -1,29 +1,22 @@
 func gcdOfStrings(str1 string, str2 string) string {
-	if len(str2) > len(str1) {
-		str1, str2 = str2, str1
-	}
-	for i := 1; i < len(str2); i++ {
-		if equalStep(str2, i) &&
-			len(str1)%(len(str2)/i) == 0 &&
-			str2[:len(str2)/i] == str1[:len(str2)/i] &&
-			equalStep(str1, i*len(str1)/len(str2)) {
-			return str2[:len(str2)/i]
+	len1 := len(str1)
+	len2 := len(str2)
+	maxLen := max(len1, len2)
+	for i := maxLen; i >= 1; i-- {
+		if len1%i == 0 && len2%i == 0 && str1[0:i] == str2[0:i] {
+			tmp1 := str1[i:] + str1[0:i]
+			tmp2 := str2[i:] + str2[0:i]
+			if tmp1 == str1 && tmp2 == str2 {
+				return str1[0:i]
+			}
 		}
 	}
+
 	return ""
 }
-
-func equalStep(str string, step int) bool {
-	if len(str)%step != 0 {
-		return false
+func max(a, b int) int {
+	if a < b {
+		return b
 	}
-
-	l := len(str) / step
-	target := str[:l]
-	for i := l; i <= len(str)-l; i = i + l {
-		if str[i:i+l] != target {
-			return false
-		}
-	}
-	return true
+	return a
 }
