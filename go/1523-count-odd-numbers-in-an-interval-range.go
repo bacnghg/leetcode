d834b5dd2a7740724ebe4acc415f9ba8f82f2f0d
bacnh7427 <bacnh7427@co-well.com.vn>
2023-02-13 10:32:44 +0700

    refactor code

--- go/1523-count-odd-numbers-in-an-interval-range.go
+++ go/1523-count-odd-numbers-in-an-interval-range.go
@@ -1,6 +1,12 @@
 func countOdds(low int, high int) int {
 	var count = 0
-	for i := low; i <= high; i++ {
+	if low%2 != 0 {
+		count++
+	}
+	if high%2 != 0 {
+		count++
+	}
+	for i := low + 1; i < high; i++ {
 		if i%2 == 1 {
 			count++
 		}
