21a3a2a0e84ed561cc166360a9cf6e5068bcf42f
bacnh7427 <bacnh7427@co-well.com.vn>
2023-02-21 17:39:44 +0700

    add note

--- go/989-add-to-array-form-of-integer.go
+++ go/989-add-to-array-form-of-integer.go
@@ -1,18 +1,33 @@
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// t := []string{"g", "h", "i"}
+	num := []int{1, 2, 0, 0}
+	k := 34
+	addToArrayForm(num, k)
+}
+
 func addToArrayForm(num []int, k int) []int {
-	result := []int{}
-	i := len(num) - 1
+	number := sliceToInt(num) + k
+	fmt.Println(number)
+	var split := strings.Split(number, "")
+	fmt.Println(split)
+	return split
+}
 
-	for i >= 0 || k > 0 {
-		if i >= 0 {
-			k = k + num[i]
-		}
-		result = append(result, k%10)
-		i--
-		k = k / 10
+func sliceToInt(s []int) int {
+	res := 0
+	op := 1
+	for i := len(s) - 1; i >= 0; i-- {
+		res += s[i] * op
+		op *= 10
 	}
-
-	for left, right := 0, len(result)-1; left < right; left, right = left+1, right-1 {
-		result[left], result[right] = result[right], result[left]
-	}
-	return result
+	return res
 }
